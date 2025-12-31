4ccfcf5602dde5e5336fb97790b6bef351db8a80
axiom-nz <2497193+axiom-nz@users.noreply.github.com>
2025-12-31 14:25:16 +1300

    Handle graceful shutdowns & terminations

--- cmd/corsway/main.go
+++ cmd/corsway/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"fmt"
@@ -8,6 +9,9 @@ import (
 	"mime"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/axiom-nz/corsway/internal/config"
 	"github.com/axiom-nz/corsway/internal/proxy"
@@ -21,6 +25,9 @@ func init() {
 }
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	// Load config with defaults
 	cfg, err := config.Load(os.Args[1:])
 	if err != nil {
@@ -35,6 +42,27 @@ func main() {
 		cfg.Port, cfg.RateLimit, cfg.RateLimitWindow, cfg.MaxRequestBytes, cfg.OriginWhitelist)
 
 	// Start server
-	server := proxy.NewServer(cfg, logger)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), server))
+	handler := proxy.NewProxy(cfg, logger)
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.Port),
+		Handler: handler,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Listen: %s\n", err)
+		}
+	}()
+
+	// Handle termination & shutdown
+	<-ctx.Done()
+	stop()
+	log.Printf("Shutting down server on port %d", cfg.Port)
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Printf("Server forced to shutdown: %v", err)
+	}
 }
